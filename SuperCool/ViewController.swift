e739e5bd70e91fc67439ba9f24c11e333ac72f4d
Angels <angels@Angelss-MBP.home>
2015-11-27 14:59:14 +0000

    Starting point

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BoomApp: UIImageView!
+    @IBOutlet weak var CoolAbs: UIImageView!
+    @IBOutlet weak var uncoolbutton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func makemenotsouncool(sender: AnyObject) {
+        BoomApp.hidden = false
+        CoolAbs.hidden = false
+        uncoolbutton.hidden = true
+    }
 
 }
 
